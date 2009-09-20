f99ca42231a85d43008196314cf711cfc6366c00
stzschoppe <stzschoppe@2bef0d2c-635e-11de-9151-f7b1c15b93db>
2009-09-20 08:50:21 +0000

    Arbeit am Datenmodell

--- fusionvis/src/de/unibw/fusionvis/common/SimpleProperty.java
+++ fusionvis/src/de/unibw/fusionvis/common/SimpleProperty.java
@@ -4,54 +4,133 @@
 package de.unibw.fusionvis.common;
 
 /**
- * <p>Eigenschaftscontainer, eine Eigenschaft mit ihrer ID, ihrem Typ und ihrem Wert speichert.
+ * <p>
+ * Eigenschaftscontainer, eine Eigenschaft mit ihrer ID, ihrem Typ und ihrem
+ * Wert speichert.
  * </p>
  * 
  * Gespeichert werden die Werte als String.
  * 
  * @author stzschoppe
- *
+ * 
  */
 public class SimpleProperty {
 	/**
 	 * ID der Eigenschaft.
 	 */
 	private String id;
-	
+
 	/**
 	 * Typ der Eigenschaft.
+	 * 
 	 * @see de.unibw.fusionvis.common.Type
 	 */
 	private Type type;
-	
+
 	/**
-	 * Wert der Eigenschaft, gespeichert in einem String Objekt. Der zugewiesene Wert
-	 * muss in den angegeben Typ überführbar sein.
+	 * Wert der Eigenschaft, gespeichert in einem String Objekt. Der zugewiesene
+	 * Wert muss in den angegeben Typ überführbar sein.
 	 */
 	private String value;
-	
+
 	/**
-	 * Konstruktor einer Eigenschaft unter angebe der erforderlichen Werte.
-	 * @param id Bezeicher 
-	 * @param type Typ
-	 * @param value Wert
+	 * Konstruktor einer Eigenschaft unter Angabe der erforderlichen Werte.
+	 * 
+	 * @param id
+	 *            Bezeicher
+	 * @param type
+	 *            Typ
+	 * @param value
+	 *            Wert
 	 */
 	public SimpleProperty(String id, Type type, String value) {
 		this.id = id;
 		this.type = type;
 		if (Type.valueCorrect(value, type)) {
 			this.value = value;
-		}
-		else this.value = Type.getNeutral(type);
-		
+		} else
+			this.value = Type.getNeutral(type);
+
+	}
+
+	/**
+	 * Konstruktor einer Eigenschaft aus einer bestehenden Eigenschaft.
+	 * 
+	 * @param sp
+	 *            Die zu kopierende Eigenschaft
+	 */
+	public SimpleProperty(SimpleProperty sp) {
+		this(sp.getId(), sp.getType(), sp.getStringValue());
 	}
 	
 	/**
-	 * Konstruktor einer Eigenschaft aus einer bestehenden Eigenschaft.
-	 * @param sp Die zu kopierende Eigenschaft
+	 * Konstruktor einer Eigenschaft unter Angabe der erforderlichen Werte.
+	 * 
+	 * @param id
+	 *            Bezeicher
+	 * @param value
+	 *            Wert
 	 */
-	public SimpleProperty(SimpleProperty sp){
-		this(sp.getId(), sp.getType(), sp.getValue());
+	public SimpleProperty(String id, int value){
+		this.id = id;
+		this.type = Type.TInt;
+		this.value = String.valueOf(value);
+	}
+	
+	/**
+	 * Konstruktor einer Eigenschaft unter Angabe der erforderlichen Werte.
+	 * 
+	 * @param id
+	 *            Bezeicher
+	 * @param value
+	 *            Wert
+	 */
+	public SimpleProperty(String id, float value){
+		this.id = id;
+		this.type = Type.TFloat;
+		this.value = String.valueOf(value);
+	}
+	
+	/**
+	 * Konstruktor einer Eigenschaft unter Angabe der erforderlichen Werte.
+	 * 
+	 * @param id
+	 *            Bezeicher
+	 * @param value
+	 *            Wert
+	 */
+	public SimpleProperty(String id, char value){
+		this.id = id;
+		this.type = Type.TChar;
+		this.value = String.valueOf(value);
+	}
+	
+	/**
+	 * Konstruktor einer Eigenschaft unter Angabe der erforderlichen Werte.
+	 * 
+	 * @param id
+	 *            Bezeicher
+	 * @param value
+	 *            Wert
+	 */
+	public SimpleProperty(String id, boolean value){
+		this.id = id;
+		this.type = Type.TBool;
+		this.value = String.valueOf(value);
+	}
+	
+	/**
+	 * Konstruktor einer Eigenschaft unter Angabe der erforderlichen Werte.
+	 * 
+	 * @param id
+	 *            Bezeicher
+	 * @param value
+	 *            Wert
+	 */
+	public SimpleProperty(String id, String value){
+		this.id = id;
+		this.type = Type.TString;
+		this.value = String.valueOf(value);
 	}
 
 	/**
@@ -71,6 +150,7 @@ public class SimpleProperty {
 
 	/**
 	 * @return Wert
+	 * @deprecated
 	 */
 	public String getValue() {
 		return value;
@@ -79,7 +159,10 @@ public class SimpleProperty {
 	/**
 	 * Setz die Eigenschaft auf einen neuen Wert. Wenn der Wert nicht zum Typ
 	 * der Eigenschaft passt, wird die Zuweisung ignoriert.
-	 * @param value the value to set
+	 * 
+	 * @param value
+	 *            the value to set
+	 * @deprecated
 	 */
 	public void setValue(String value) {
 		if (Type.valueCorrect(value, type)) {
@@ -87,7 +170,9 @@ public class SimpleProperty {
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#hashCode()
 	 */
 	@Override
@@ -100,7 +185,9 @@ public class SimpleProperty {
 		return result;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
@@ -130,13 +217,116 @@ public class SimpleProperty {
 		return true;
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * Gibt eine Integerrepräsentation der Eigenschaft wieder.
+	 * 
+	 * @return Integerwert der Eigenschaft, oder 0, falls kein gültiger Wert.
+	 */
+	public int getIntValue() {
+		if (getType() == Type.TInt) {
+			return Integer.valueOf(value);
+		} else {
+			return Integer.valueOf(Type.getNeutral(Type.TInt));
+		}
+	}
+
+	/**
+	 * Gibt eine Floatrepräsentation der Eigenschaft wieder.
+	 * 
+	 * @return Floatwert der Eigenschaft, oder 0.0, falls kein gültiger Wert.
+	 */
+	public float getFloatValue() {
+		if (getType() == Type.TFloat) {
+			return Float.valueOf(value);
+		} else {
+			return Float.valueOf(Type.getNeutral(Type.TFloat));
+		}
+	}
+
+	/**
+	 * Gibt eine Charrepräsentation der Eigenschaft wieder.
+	 * 
+	 * @return Charwert der Eigenschaft, oder "\0", falls kein gültiger Wert.
+	 */
+	public char getCharValue() {
+		if (getType() == Type.TChar) {
+			return value.charAt(0);
+		} else {
+			return Type.getNeutral(Type.TChar).charAt(0);
+		}
+	}
+
+	/**
+	 * Gibt eine Boolrepräsentation der Eigenschaft wieder.
+	 * 
+	 * @return Boolwert der Eigenschaft, oder <code>false</code>, falls kein
+	 *         gültiger Wert.
+	 */
+	public boolean getBoolValue() {
+		if (getType() == Type.TBool) {
+			return Boolean.valueOf(value);
+		} else {
+			return Boolean.valueOf(Type.getNeutral(Type.TBool));
+		}
+	}
+
+	/**
+	 * Gibt eine Stringrepräsentation der Eigenschaft wieder.
+	 * 
+	 * @return Stringwert der Eigenschaft.
+	 */
+	public String getStringValue() {
+		return value;
+	}
+	
+	/**
+	 * Setzt die Eigenschaft mit einem Integerwert. Fals der Typ nicht mit dem Typ der
+	 * Eigenschaft übereinstimmt, geschiet keine Änderung.
+	 * @param newValue Der neue Wert.
+	 */
+	public void set_Value(int newValue) {
+		if (getType() == Type.TInt) 
+			value = String.valueOf(newValue);
+	}
+	
+	/**
+	 * Setzt die Eigenschaft mit einem Floatwert. Fals der Typ nicht mit dem Typ der
+	 * Eigenschaft übereinstimmt, geschiet keine Änderung.
+	 * @param newValue Der neue Wert.
+	 */
+	public void set_Value(float newValue) {
+		if (getType() == Type.TFloat) 
+			value = String.valueOf(newValue);
+	}
+	
+	/**
+	 * Setzt die Eigenschaft mit einem Charwert. Fals der Typ nicht mit dem Typ der
+	 * Eigenschaft übereinstimmt, geschiet keine Änderung.
+	 * @param newValue Der neue Wert.
+	 */
+	public void set_Value(char newValue) {
+		if (getType() == Type.TChar) 
+			value = String.valueOf(newValue);
+	}
+	
+	/**
+	 * Setzt die Eigenschaft mit einem Boolwert. Fals der Typ nicht mit dem Typ der
+	 * Eigenschaft übereinstimmt, geschiet keine Änderung.
+	 * @param newValue Der neue Wert.
+	 */
+	public void set_Value(boolean newValue) {
+		if (getType() == Type.TBool) 
+			value = String.valueOf(newValue);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		return id + " [type=" + type + ", value="
-				+ value + "]";
+		return id + " [type=" + type + ", value=" + value + "]";
 	}
-	
+
 }
