8bd5e00c21d04a07915e8dd02cd90bb1fe967e7e
stzschoppe <stzschoppe@2bef0d2c-635e-11de-9151-f7b1c15b93db>
2009-10-31 16:17:20 +0000

    Umstellung auf das neue Datenmodell

--- trunk/fusionvis/src/de/unibw/fusionvis/importer/Importer.java
+++ trunk/fusionvis/src/de/unibw/fusionvis/importer/Importer.java
@@ -3,7 +3,16 @@
  */
 package de.unibw.fusionvis.importer;
 
+import org.w3c.dom.Node;
+
 import de.unibw.fusionvis.common.DataSet;
+import de.unibw.fusionvis.common.Type;
+import de.unibw.fusionvis.common.properties.AbstractProperty;
+import de.unibw.fusionvis.common.properties.BooleanProperty;
+import de.unibw.fusionvis.common.properties.CharProperty;
+import de.unibw.fusionvis.common.properties.FloatProperty;
+import de.unibw.fusionvis.common.properties.IntProperty;
+import de.unibw.fusionvis.common.properties.StringProperty;
 
 /**
  * <p>Baut einen DOM-Baum aus einem XML-Datensatz und wandelt diesen in 
@@ -40,6 +49,28 @@ public abstract class Importer {
 	 */
 	public abstract DataSet getDataSet();
 	
-
+	/**
+	 * Extrahiert eine einfache Eigenschaft.
+	 * @param node XML-Node
+	 * @param type Einer der folgenden Typen: <code>TString, TBool, TFloat, TInt, TChar</code>
+	 * @return Die jeweilige Typ-Implementierung der AbstractProperty oder null, wenn
+	 * keiner der o. a. Typen verwendet wurde
+	 */
+	protected AbstractProperty extractSimpleProperty(Node node, Type type) {
+		switch (type) {
+		case TString:
+			return new StringProperty(node.getNodeName(), node.getTextContent());
+		case TBool:
+			return new BooleanProperty(node.getNodeName(), Boolean.parseBoolean(node.getTextContent()));
+		case TFloat:
+			return new FloatProperty(node.getNodeName(), Float.parseFloat(node.getTextContent()));
+		case TInt:
+			return new IntProperty(node.getNodeName(), Integer.parseInt(node.getTextContent()));
+		case TChar:
+			return new CharProperty(node.getNodeName(), node.getTextContent().charAt(0));
+		default:
+			return null;
+		}
+	}
 
 }
