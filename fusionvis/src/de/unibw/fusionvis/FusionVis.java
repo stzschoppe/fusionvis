89e508763345e82816f39e2a83761f4c4a34a04e
stzschoppe <stzschoppe@2bef0d2c-635e-11de-9151-f7b1c15b93db>
2009-10-31 17:12:54 +0000

    shell mit filter, printAll und exit integriert

--- fusionvis/src/de/unibw/fusionvis/FusionVis.java
+++ fusionvis/src/de/unibw/fusionvis/FusionVis.java
@@ -2,6 +2,13 @@ package de.unibw.fusionvis;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import shell.Command;
+import shell.CommandHandler;
+import shell.IntegerParameter;
+import shell.Shell;
+import shell.StringParameter;
+import shell.tools.HaltCommand;
+
 import de.unibw.fusionvis.common.DataSet;
 import de.unibw.fusionvis.importer.BattleSimImporter;
 import de.unibw.fusionvis.importer.Importer;
@@ -20,6 +27,12 @@ public class FusionVis {
 	/** Importer zum Erstellen der Datenstruktur*/
 	private Importer importer;
 	
+	/**
+	 * Konstrucktor
+	 */
+	public FusionVis() {
+		createCommands();
+	}
 	
 	/**
 	 * Getter fuer den globalen Logger
@@ -38,22 +51,16 @@ public class FusionVis {
 		try {
 			// Logger Konfiguration
 			logger.setLevel(Level.ALL);
-			logger.log(Level.INFO, "starte FusionVis..\n");
-
-			// Initialisierung des Importers
-			logger.log(Level.INFO, "Initialisierung des Importers" + "\n");
-			fusionVis.importer = new BattleSimImporter("\\res\\sit8979.xml"); 
+			logger.log(Level.INFO, "starte FusionVis..\n");	
 			
-			DataSet dataSet = fusionVis.importer.getDataSet().filterBy("Certainty", "Perceived");
-			dataSet = dataSet.filterBy("HostilityCode", "HO");
 			
-			logger.log(Level.INFO, "\n"
-					+ fusionVis.importer.getDataSet().getData().size()
-					+ " Datensätze Importiert.\n");
-			logger.log(Level.INFO, dataSet + "\n");
-			logger.log(Level.INFO, ""
-					+ dataSet.getData().size()
-					+ " Datensätze gefiltert.\n");
+			Shell.getInstance().run();
+			
+			
+			
+			
+//			DataSet dataSet = fusionVis.importer.getDataSet().filterBy("Certainty", "Perceived");
+//			dataSet = dataSet.filterBy("HostilityCode", "HO");
 			
 			// Beenden
 			logger.log(Level.INFO, "beende FusionVis");
@@ -61,5 +68,75 @@ public class FusionVis {
 			logger.log(Level.SEVERE, e.getMessage());
 			logger.log(Level.INFO, "beende FusionVis");
 		}
+	}
+
+	private void importXML(String file) {
+		// Initialisierung des Importers
+		logger.log(Level.INFO, "Initialisierung des Importers mit " + file + "\n");
+		this.importer = new BattleSimImporter(file);
+		logger.log(Level.INFO, "\n"
+				+ this.importer.getDataSet().getData().size()
+				+ " Datensätze importiert.\n");
+	}
+
+	/**
+	 * Gibt einen Datensatz über den Logger aus
+	 * @param dataSet
+	 */
+	private void printAll() {
+		logger.log(Level.INFO, this.importer.getDataSet() + "\n");
+		logger.log(Level.INFO, ""
+				+ this.importer.getDataSet().getData().size()
+				+ " Datensätze vorhanden.\n");
+	}
+	
+	/**
+     * Erstellt die commands für die shell. Ausgelagert aus dem Konstruktor auf Gruenden der Uebersicht.
+     * Weiterfuehrende Erlaeuterungen zu finden in der shell/package.html.
+     */
+    private void createCommands(){ 	
+    	//Erstellt ein Kommando ohne Parameter
+    	Shell.getInstance().addCommand(new Command("printAll", "Gibt alle eingelesenen Datensätze aus", new CommandHandler(){
+    		
+			@Override
+			public void execute(Command command) {
+				printAll();
+			}}));
+    	
+    	
+    	//Erstellt ein Kommando zum Beenden von FusionVis
+    	Shell.getInstance().addCommand(new HaltCommand("exit", "beendet FusionVis"));
+    	
+    	
+    	//Erstellt ein Kommando zum Importieren von XML-Dateien
+    	Command importXML = (new Command("import", "Läd eine angegebene xml-Datei.", new CommandHandler(){
+    		
+			@Override
+			public void execute(Command command) {
+				importXML(command.getParameterByName("file").getValueAsString());				
+			}}));
+    	//Fügt dem Kommando einen Parameter hinzu
+    	importXML.addParameter(new StringParameter("file", "Dateiname der zu importierenden XML-Datei."));	
+    	Shell.getInstance().addCommand(importXML);
+    	
+    	
+    	//Erstellt ein Kommando zum Importieren von XML-Dateien
+    	Command filter = (new Command("filter", "Filtert den eingelesenen Datensatz", new CommandHandler(){
+    		
+			@Override
+			public void execute(Command command) {
+				filter(command.getParameterByName("key").getValueAsString(), command.getParameterByName("value").getValueAsString());				
+			}}));
+    	//Fügt dem Kommando zwei Parameter hinzu
+    	filter.addParameter(new StringParameter("key", "Schlüssel, nach dessen Wert zu filtern ist"));
+    	filter.addParameter(new StringParameter("value", "Wert, nachdem gefiltert werden soll"));
+    	Shell.getInstance().addCommand(filter);
+    }
+
+	private void filter(String key, String value) {
+		importer.setDataSet(importer.getDataSet().filterBy(key, value));
+		logger.log(Level.INFO, ""
+				+ this.importer.getDataSet().getData().size()
+				+ " Datensätze vorhanden.\n");
 	}
 }
