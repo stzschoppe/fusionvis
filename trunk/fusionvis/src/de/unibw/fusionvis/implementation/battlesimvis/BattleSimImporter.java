7c6e71f9ff2c4ba474c7f916747b56437f9f4d73
stzschoppe <stzschoppe@2bef0d2c-635e-11de-9151-f7b1c15b93db>
2009-11-01 15:31:55 +0000

    refactoring der Importer

--- trunk/fusionvis/src/de/unibw/fusionvis/implementation/battlesimvis/BattleSimImporter.java
+++ trunk/fusionvis/src/de/unibw/fusionvis/implementation/battlesimvis/BattleSimImporter.java
@@ -26,149 +26,97 @@ import de.unibw.fusionvis.common.properties.ContainerProperty;
 import de.unibw.fusionvis.importer.Importer;
 
 /**
- * <p>Baut einen DOM-Baum aus einem XML-Datensatz eines Battle Simulators 
- * und wandelt diesen in eine Collection von einzelnen Dateneinträgen um.</p>
+ * <p>
+ * Baut einen DOM-Baum aus einem XML-Datensatz eines Battle Simulators und
+ * wandelt diesen in eine Collection von einzelnen Dateneinträgen um.
+ * </p>
+ * 
  * @author stzschoppe
  */
 public class BattleSimImporter extends Importer {
 	private Logger logger;
-	
+
 	/**
-	 * Konstruktor eines Importers für Ausgaben eines Battle Simulators 
-	 * unter Angabe der zu importierenden XML-Datei
-	 * @param xmlDataLocation Pfad zur XML-Datei
+	 * Konstruktor eines Importers für Ausgaben eines Battle Simulators unter
+	 * Angabe der zu importierenden XML-Datei
+	 * 
+	 * @param logger Übergeordneter Logger
+	 *            Pfad zur XML-Datei
 	 */
-	public BattleSimImporter(String xmlDataLocation, Logger logger) {
-		this.logger = logger;
+	public BattleSimImporter(Logger logger) {
+		super(logger);
 		id = "Name";
 		position = "Location";
-		
+
 		// Listen der zu übernehmenden Elemente initialisieren.
 		simplePropertyList.add("FormalAbbrdName");
-//		simplePropertyList.add("SisoEbv");
+		// simplePropertyList.add("SisoEbv");
 		simplePropertyList.add("IsPlatform");
 		simplePropertyList.add("HostilityCode");
-//		simplePropertyList.add("AffiliationCode");
-//		simplePropertyList.add("SizeCode");
-//		simplePropertyList.add("OperStateCode");
-//		simplePropertyList.add("TypeId");
-//		simplePropertyList.add("SymbolId");
+		// simplePropertyList.add("AffiliationCode");
+		// simplePropertyList.add("SizeCode");
+		// simplePropertyList.add("OperStateCode");
+		// simplePropertyList.add("TypeId");
+		// simplePropertyList.add("SymbolId");
 		simplePropertyList.add("Certainty");
 		simplePropertyList.add("Reliability");
 		simplePropertyList.add("LastModified");
-		
+
 		vectorPropertyList.add("Velocity");
 		vectorPropertyList.add("Orientation");
 	}
 
 	@Override
-	public void runImport(String file) {
-				// Einlesen des Datensatzes
-				DocumentBuilderFactory factory = DocumentBuilderFactory
-						.newInstance();
-				DocumentBuilder builder = null;
-				try {
-					builder = factory.newDocumentBuilder();
-					document = builder.parse(file);
-					buildDataSet();
-
-				} catch (ParserConfigurationException e) {
-					logger.log(
-							Level.SEVERE,
-							"Fehler beim Initialisieren des Importers" + "\n"
-									+ e.getLocalizedMessage() + "\n");
-				} catch (SAXException e) {
-					logger.log(
-							Level.SEVERE,
-							"Fehler beim Initialisieren des Importers " + "\n"
-									+ e.getLocalizedMessage() + "\n");
-				} catch (IOException e) {
-					logger.log(
-							Level.SEVERE,
-							"Fehler beim Initialisieren des Importers " + "\n"
-									+ e.getLocalizedMessage() + "\n");
-				}
-
-	}
-	
-	/**
-	 * Erzeugt einen Satz von Daten
-	 */
-	private void buildDataSet() {
-		Node root = document.getDocumentElement();
-		Node current = root;
-		
-		//Bezeicher für dataSet auslesen
-		dataSet = new DataSet(root.getNodeName());
-		
-		current = current.getFirstChild().getNextSibling(); // current --> <Units>
-		NodeList units = current.getChildNodes();
-		
-		for (int i=0; i<units.getLength();i++){
-			if (units.item(i).getNodeType() != Node.ELEMENT_NODE) {
-				continue; //wenn kein Element, dann skip
-			}
-			dataSet.addData(extractUnit(units.item(i)));
-		}
-	}
-	
-	/**
-	 * Hilfsfuktion zum Extrahieren der Units
-	 * @param unitNode DOM Node &lt;Unit>
-	 * @return Data-Objekt
-	 */
-	private Data extractUnit(Node unitNode) {
+	protected Data extractUnit(Node unitNode) {
 		Data result = null;
 		NodeList list = unitNode.getChildNodes();
-		
+
 		// Erster Durchlauf zum Finden des Bezeichners
-		for (int i=0; i<list.getLength();i++){
+		for (int i = 0; i < list.getLength(); i++) {
 			if (list.item(i).getNodeType() != Node.ELEMENT_NODE) {
-				continue; //wenn kein Element, dann skip
+				continue; // wenn kein Element, dann skip
 			}
-				Node element = list.item(i);
-				if (element.getNodeName().equals(id)) {
-					result = new Data(element.getTextContent());
-				}
+			Node element = list.item(i);
+			if (element.getNodeName().equals(id)) {
+				result = new Data(element.getTextContent());
+			}
 		}
-		
-		//Zweiter Durchlauf für das setzen der restlichen Eigenschaften
-		for (int i=0; i<list.getLength();i++){
+
+		// Zweiter Durchlauf für das setzen der restlichen Eigenschaften
+		for (int i = 0; i < list.getLength(); i++) {
 			if (list.item(i).getNodeType() != Node.ELEMENT_NODE) {
-				continue; //wenn kein Element, dann skip
+				continue; // wenn kein Element, dann skip
 			}
-				Node element = list.item(i);
-				//Position setzen
-				if (element.getNodeName().equals(position)) {
-					result.setPosition(extractContainerProperty(element, "Position"));
-				}
-				else if (simplePropertyList.contains(element.getNodeName())) {
-					if (element.getNodeName().equals("IsPlatform")) {
-						result.addAbstractProperty(extractSimpleProperty(element,
-								Type.TBool));
-					}
-					else result.addAbstractProperty(extractSimpleProperty(element,
+			Node element = list.item(i);
+			// Position setzen
+			if (element.getNodeName().equals(position)) {
+				result
+						.setPosition(extractContainerProperty(element,
+								"Position"));
+			} else if (simplePropertyList.contains(element.getNodeName())) {
+				if (element.getNodeName().equals("IsPlatform")) {
+					result.addAbstractProperty(extractSimpleProperty(element,
+							Type.TBool));
+				} else
+					result.addAbstractProperty(extractSimpleProperty(element,
 							Type.TString));
-				} 
-				else if (vectorPropertyList.contains(element.getNodeName())) {
-					result.addContainerProperty(extractContainerProperty(element, element.getNodeName()));
-				}
-				else if (taxonomyList.contains(element.getNodeName())) {
-					result.addTaxonomie((extractSimpleProperty(element)));
-				}
-				else {
-					//skip
-				}
+			} else if (vectorPropertyList.contains(element.getNodeName())) {
+				result.addContainerProperty(extractContainerProperty(element,
+						element.getNodeName()));
+			} else if (taxonomyList.contains(element.getNodeName())) {
+				result.addTaxonomie((extractSimpleProperty(element)));
+			} else {
+				// skip
+			}
 		}
-		
+
 		return result;
 	}
-	
 
-	
-	
 	/**
-	 * Extrahiert eine einfache Eigenschaft vom Typ Float, fallsmöglich, String sonst.
+	 * Extrahiert eine einfache Eigenschaft vom Typ Float, fallsmöglich, String
+	 * sonst.
+	 * 
 	 * @param node
 	 * @return
 	 */
@@ -180,9 +128,10 @@ public class BattleSimImporter extends Importer {
 			return extractSimpleProperty(node, Type.TString);
 		}
 	}
-	
+
 	/**
 	 * Extrahiert eine Vektorielle Eigenschaft.
+	 * 
 	 * @param node
 	 * @param id
 	 * @return
@@ -190,14 +139,14 @@ public class BattleSimImporter extends Importer {
 	private ContainerProperty extractContainerProperty(Node node, String id) {
 		ContainerProperty result = new ContainerProperty(id);
 		NodeList list = node.getChildNodes();
-		
-		for (int i=0; i<list.getLength();i++){
+
+		for (int i = 0; i < list.getLength(); i++) {
 			if (list.item(i).getNodeType() != Node.ELEMENT_NODE) {
-				continue; //wenn kein Element, dann skip
+				continue; // wenn kein Element, dann skip
 			}
-				result.addComponent(extractSimpleProperty(list.item(i)));
+			result.addComponent(extractSimpleProperty(list.item(i)));
 		}
-		
+
 		return result;
 	}
 
